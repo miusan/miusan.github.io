b279e40cdbaa1b08f0ad2845fff559fea3dfe0d8
miusan <haru0006151999@yahoo.co.jp>
2017-03-07 15:28:30 +0900

    ^-^

---
+++ style.cs
@@ -0,0 +1,15 @@
+*{
+  text-align:center;
+}
+h1{
+    color: red;
+    background-color : blue;
+  }
+h2{
+   color: yellow;
+    background-color : lightgreen;
+}
+h3{
+    color: white;
+    background-color : black;
+  }
